f5b4bceae68a80c91e844da0d7a39b07468c683b
Vadim Tikhanoff <vadim.tikhanoff@iit.it>
2018-02-10 15:46:40 +0100

    Update test.sh

--- smoke-test/test.sh
+++ smoke-test/test.sh
@@ -39,8 +39,8 @@ build_dir=$(pwd)
 
 if [ $? -eq 0 ]; then
     if [ -f ${test_dir}/test-type ]; then
-        test_type=$(head -1 ${test_dir}/test-type)
-        ./helpers/scripts/smoke-test-${test_type}.sh $build_dir $code_dir $test_dir
+        test_type=$(head -1 "${test_dir}"/test-type)
+        ./helpers/scripts/smoke-test-${test_type}.sh "$build_dir" "$code_dir" "$test_dir"
         ret=$?
     else
         echo -e "${red}test-type is missing!${nc}"
